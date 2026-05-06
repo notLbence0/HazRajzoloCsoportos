342087cd9340e1b6cbbe5d3a68f27ae77cead339
notLbence0 <bencel1003@gmail.com>
2026-05-06 10:57:48 +0200

    házjavitás

--- HazRajzolo.py
+++ HazRajzolo.py
@@ -1,18 +1,24 @@
-#haromszog
 import turtle
 
+turtle.color("red")
+turtle.pensize(5)
+turtle.speed(0)
+t = turtle.Turtle()
+
+#negyzet
 i=0
-
-turtle.left(45)
-while i<3:
-    turtle.forward(300)
-    turtle.right(45)
-
-#hazalja
 while i < 4:
-    turtle.forward(300)
+    turtle.forward(200)
     turtle.left(-90)
-
     i+=1
+
+#hazteteje
+t.pensize(5)
+t.color("red")
+t.setheading(60)
+t.forward(200)
+t.setheading(-60)
+t.forward(200)
+
 
 turtle.done()
